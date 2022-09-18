088e4e631b8edf41a6f4b3a7cf7242db422e5086
n4rwal <zero9691@yahoo.it>
2022-09-18 11:33:03 +0200

    mouse cursor added

---
+++ scripts/Cursore.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
+
+func _process(delta):
+	self.position = self.get_global_mouse_position()
