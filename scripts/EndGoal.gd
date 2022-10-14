8bf95a7aac26008ae4eab6cd07ccb7717617bcf3
Christer Kaitila <breakdance@mcfunkypants.com>
2022-10-14 15:06:33 -0700

    black hole vortex effect v2

--- scripts/EndGoal.gd
+++ scripts/EndGoal.gd
@@ -1,5 +1,10 @@
 extends Area
 
+func _process(delta):
+	# spin around
+	var rot_speed = rad2deg(0.1)  # 30 deg/sec
+	rotate_y(rot_speed * delta)
+
 func _on_EndGoal_body_entered(body: Node) -> void:
 	if body.is_in_group("box"):
 		$Popup.popup()
