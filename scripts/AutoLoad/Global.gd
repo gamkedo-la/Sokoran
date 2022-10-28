ef5de5817a5ad1a654d68d68f9f7e91d39529672
Oli Brock <kbshields@gmail.com>
2022-10-28 13:22:09 -0400

    Loading Bar only shows if 1sec of load time passes.

--- scripts/AutoLoad/Global.gd
+++ scripts/AutoLoad/Global.gd
@@ -2,7 +2,8 @@ extends Node
 
 
 export var max_load_time = 100000000
-
+var show_bar = false
+var show_bar_time = 100
 var running_scene = null
 
 var levels = [
@@ -10,6 +11,9 @@ var levels = [
 	"res://scenes/LVLone.tscn",
 	"res://scenes/LVLone.tscn"
 ]
+
+var loading_bar
+var loading_bar_inst
 
 var current_level = 0
 
@@ -25,18 +29,15 @@ func goto_scene(path, current_scene):
 	if loader == null:
 		print("Resource loaded unable to load the resource at path")
 		return
-	var loading_bar_inst = load("res://scenes/UI/LoadingBar.tscn").instance()
-	
-	get_tree().get_root().call_deferred('add_child',loading_bar_inst)
-	var loading_bar = loading_bar_inst.get_node_or_null("%ProgressBar")
-	if loading_bar == null:
-		print("Could not find loading bar scene.")
-		return
+
 		
 	
 	var t = OS.get_ticks_msec()
 	
 	while OS.get_ticks_msec() - t < max_load_time:
+		if OS.get_ticks_msec() - t > show_bar_time && !show_bar:
+			load_bar()
+			show_bar = true
 		var err = loader.poll()
 		if err == ERR_FILE_EOF:
 			#Loading Complete
@@ -44,14 +45,27 @@ func goto_scene(path, current_scene):
 			get_tree().get_root().call_deferred('add_child',resource.instance())
 			
 			current_scene.queue_free()
-			loading_bar_inst.queue_free()
+			if show_bar:
+				loading_bar = null
+				loading_bar_inst.queue_free()
+				show_bar = false
 			break
 		elif err == OK:
 			#Still loading
-			var progress = float(loader.get_stage())/loader.get_stage_count()
-			loading_bar.value = progress * 100
-			print(progress)
+			if show_bar:
+				var progress = float(loader.get_stage())/loader.get_stage_count()
+				loading_bar.value = progress * 100
+				print(progress)
 		else:
 			print("Error while loading file")
 			break
 		yield(get_tree(),"idle_frame")
+		
+func load_bar() -> void:
+	loading_bar_inst = load("res://scenes/UI/LoadingBar.tscn").instance()
+	
+	get_tree().get_root().call_deferred('add_child',loading_bar_inst)
+	loading_bar = loading_bar_inst.get_node_or_null("%ProgressBar")
+	if loading_bar == null:
+		print("Could not find loading bar scene.")
+		return
